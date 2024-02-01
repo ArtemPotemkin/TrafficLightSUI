142aa8fdfb7e7378d79431f28bc28565253cfe51
Artem Potemkin <art.potemkin@gmail.com>
2024-02-02 00:14:07 +0300

    Initial Commit

---
+++ TrafficLightSUI/TrafficLightSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSUIApp.swift
+//  TrafficLightSUI
+//
+//  Created by Артём Потёмкин on 02.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
